1ceb288b0c4edc8177514c8c880b55d3c4ef7d41
Astrella.Ne <mycuttingapp@gmail.com>
2023-11-01 21:48:38 +0300

    refactored brain_start attempt 4

--- brain_games/scripts/brain_start.py
+++ brain_games/scripts/brain_start.py
@@ -19,35 +19,37 @@ games = {
 
 
 def game_choice(name):
-    max_attempts = 3
-    available_choices = {'1': "Even or Odd",
-                         '2': "Calculator",
-                         '3': "Common Divisor",
-                         '4': "Progression",
-                         '5': "Prime Number"}
+    # Счетчик неверного ввода выбора игры
+    non_digit_choice = 0
+    chosen_game = None
+    
+    while chosen_game is None:
+        choice = prompt.string('''Choose a game: 
+        Print "1" for "Even or Odd"
+        Print "2" for "Calculator"
+        Print "3" for "Common Divisor"
+        Print "4" for "Progression"
+        Print "5" for "Prime Number":''')
 
-    for attempt in range(1, max_attempts + 1):
-        choice = prompt.string('''Choose a game:
-            Print "1" for "Even or Odd"
-            Print "2" for "Calculator"
-            Print "3" for "Common Divisor"
-            Print "4" for "Progression"
-            Print "5" for "Prime Number":''')
-
-        if choice in available_choices:
-            chosen_game = games.get(choice)
-            return chosen_game
-
-        print(f'Invalid choice, {name}!'
-              f'Please enter a number of the available games.')
-
-        if attempt == max_attempts:
-            print(f'''Sorry, {name}, too many mistakes ;(.
+        if choice.isdigit():
+            if choice in games:
+                chosen_game = games.get(choice)
+            else:
+                print(f'Invalid choice! Please enter a number corresponding to the available games.')
+                non_digit_choice += 1
+                if non_digit_choice == 3:
+                    print(f'''Sorry, {name}, too many mistakes ;(.
 We end the game. Let's try again next time!''')
-            return None
-
-        print('Digits only!')
-    return None
+                    return
+        else:
+            print('Digits only!')
+            non_digit_choice += 1
+            if non_digit_choice == 3:
+                print(f'''Sorry, {name}, too many mistakes ;(.
+We end the game. Let's try again next time!''')
+                return
+    
+    return chosen_game
 
 
 def game_cycle(chosen_game):
